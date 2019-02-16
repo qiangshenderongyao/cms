99e0661889048897d23fbad9af2e04eae9db75aa
qiangshenderongyao <2302938274@qq.com>
2019-02-16 08:33:17 +0000

    微信1

--- routes/web.php
+++ routes/web.php
@@ -15,22 +15,8 @@ Route::get('/', function () {
     // echo date('Y-m-d H:i:s');
     return view('1805');
 });
-<<<<<<< HEAD
 Route::get('/info',function(){
     phpinfo();
-=======
-//查看扩展
-Route::get('/info', function () {
-<<<<<<< HEAD
-	phpinfo();
-});
-
-Route::middleware(['log.click'])->group(function(){
-	Route::get('/test/cookie1','Ce\CeController@cookieTest1');
-=======
-    phpinfo();
->>>>>>> houtai
->>>>>>> ea52b742f57db35e614306f39a32672887952f17
 });
 //Route::post('/abc/{id}/{name}','TestController@test')->where('id','\d+')->name('a');
 Route::post('/add','TestController@add');
@@ -40,7 +26,7 @@ Route::get('/update','TestController@update');
 Route::post('/update_add','TestController@update_add');
 Route::any('/zhu','Vip\IndexController@zhu');
 Route::any('/zhuce','Vip\IndexController@zhuce');
-Route::any('/login','Vip\IndexController@login')->name('login');
+Route::any('/login','Vip\IndexController@login');
 Route::post('/loginadd','Vip\IndexController@loginadd');
 Route::any('/ce','Vip\IndexController@ce');
 Route::any('/center','Vip\IndexController@center');
@@ -51,16 +37,11 @@ Route::get('/delete/{goods_id}','Cart\IndexController@delete')->middleware('chec
 Route::get('/request','Cart\IndexController@cart')->middleware('check.login.token');
 Route::get('/goodsadd/{goods_id}','Good\GoodsController@goodsadd');
 Route::get('/goods','Good\GoodsController@goods');
-<<<<<<< HEAD
 Route::get('/goodsa','Good\GoodsController@goodsa');
 Route::post('/Redisbuy','Good\GoodsController@Redisbuy');
 Route::get('/Rediszuo','Good\GoodsController@Rediszuo');
 Route::get('/updateGoodsInfo','Good\GoodsController@updateGoodsInfo');
 Route::post('/goodsou','Good\GoodsController@goodsou');
-=======
-Route::get('/upload','Good\GoodsController@upload');
-Route::post('/uploadpdf','Good\GoodsController@uploadpdf');
->>>>>>> ea52b742f57db35e614306f39a32672887952f17
 Route::get('/del/{goods_id}','Cart\IndexController@del');
 Route::any('/add2','Cart\IndexController@add2');
 Route::get('/order','Order\OrderController@order');
@@ -75,12 +56,8 @@ Route::post('/pay/alipay/notify','Pay\AlipayController@notify');
 Route::get('/pay/alipay/return','Pay\AlipayController@Return');
 Route::get('/pay/alipay/orderdelete','Pay\AlipayController@orderdelete');
 Auth::routes();
-<<<<<<< HEAD
 
 Route::get('/home', 'HomeController@index')->name('home');
 //微信
 Route::get('/weixin','weixin\WeixinController@validToken1');
 Route::post('/weixin/valid1','weixin\WeixinController@wxEvent');
-=======
-Route::get('/home', 'HomeController@index')->name('home');
->>>>>>> ea52b742f57db35e614306f39a32672887952f17

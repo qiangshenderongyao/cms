04eee8259ed6243c3e0da6586a28e7377405bd33
qiangshen <2302938274@qq.com>
2019-02-16 09:39:16 +0000

    .env

--- app/Http/Controllers/weixin/WeixinController.php
+++ app/Http/Controllers/weixin/WeixinController.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Redis;
 class WeixinController extends Controller{
+    protected $redis_weixin_access_token = 'str:weixin_access_token';     //微信 access_token
     //首次接入
     function validToken1(){
         echo $_GET['echostr'];
@@ -15,5 +16,22 @@ class WeixinController extends Controller{
         $data=file_get_contents("php:input");
         $log=date('Y:m:d H:i:s')."\n".$data."\n<<<<<";
         file_put_contents('logs/wx_event.log',$log,FILE_APPEND);
+    }
+    public function getWXAccessToken()
+    {
+
+        //获取缓存
+        $token = Redis::get($this->redis_weixin_access_token);
+        if(!$token){        // 无缓存 请求微信接口
+            $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.env('WEIXIN_APPID').'&secret='.env('WEIXIN_APPSECRET');
+            $data = json_decode(file_get_contents($url),true);
+
+            //记录缓存
+            $token = $data['access_token'];
+            Redis::set($this->redis_weixin_access_token,$token);
+            Redis::setTimeout($this->redis_weixin_access_token,3600);
+        }
+        return $token;
+
     }
 }

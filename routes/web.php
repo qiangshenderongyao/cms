dd4b1f80a4c5d761c0161f8820b1790bea74ebf0
qiangshenderongyao <2302938274@qq.com>
2019-02-18 02:27:05 +0000

    emm

--- routes/web.php
+++ routes/web.php
@@ -59,9 +59,9 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 //微信
+Route::post('/weixin','weixin\WeixinController@validToken');
 Route::get('/weixin/test','weixin\WeixinController@test');
 Route::get('/weixin/valid','weixin\WeixinController@validToken');
 Route::get('/weixin/valid1','weixin\WeixinController@validToken1');
 Route::post('/weixin/valid1','weixin\WeixinController@weixinEven');
 Route::post('/weixin/valid','weixin\WeixinController@validToken');
-

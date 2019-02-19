2696c24554bc33d704368da44a4afb6fd7a7a06e
qiangshenderongyao <2302938274@qq.com>
2019-02-19 07:43:17 +0000

    aq

--- app/Http/Controllers/weixin/WeixinController.php
+++ app/Http/Controllers/weixin/WeixinController.php
@@ -199,26 +199,31 @@ class WeixinController extends Controller{
                         ]
                     ]
                 ],
-                ["name"=>"发图",
-                    "sub_button"=>[
-                        [
-                            "type"=>"pic_sysphoto",
-                            "name"=>"系统拍照发图",
-                            "key"=>"rselfmenu_1_0",
-                            "sub_button"=>[]
-                        ],
-                        [
-                            "type"=>"pic_photo_or_album",
-                            "name"=>"拍照或相册发图",
-                            "key"=>"rselfmenu_1_1",
-                            "sub_button"=>[]
-                        ]
-                    ]
-                ],
+//                ["name"=>"发图",
+//                    "sub_button"=>[
+//                        [
+//                            "type"=>"pic_sysphoto",
+//                            "name"=>"系统拍照发图",
+//                            "key"=>"rselfmenu_1_0",
+//                            "sub_button"=>[]
+//                        ],
+//                        [
+//                            "type"=>"pic_photo_or_album",
+//                            "name"=>"拍照或相册发图",
+//                            "key"=>"rselfmenu_1_1",
+//                            "sub_button"=>[]
+//                        ]
+//                    ]
+//                ],
                 [
                     "type"  => "click",      // click类型
                     "name"  => "时间",
                     "key"   => "kefu01"
+                ],
+                [
+                "type"=> "click",      // click类型
+                "name"  => "图片",
+                "key"   => "kefu01"
                 ]
             ]
         ];

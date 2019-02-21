71fda5b9281a72b984df42197a7da5e946208c51
qiangshenderongyao <2302938274@qq.com>
2019-02-21 00:59:54 +0000

    a

--- app/Admin/Controllers/WeixinMediaController.php
+++ app/Admin/Controllers/WeixinMediaController.php
@@ -89,7 +89,7 @@ class WeixinMediaController extends Controller
         $grid->format('Format');
         $grid->msg_id('Msg id');
         $grid->local_file_name('Local file name')->display(function($wx_imgage_put){
-            return '<img src="http://1807.96myshop.cn/wx/images/'.$wx_imgage_put.'">';
+            return '<img src="http://1807.96myshop.cn/'.$wx_imgage_put.'">';
         });
         $grid->local_file_path('Local file path');
 

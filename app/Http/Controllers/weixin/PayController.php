10277ec5ddc27e50ee29afa18aaf3a2523355842
qiangshenderongyao <2302938274@qq.com>
2019-02-26 10:03:53 +0000

    a

--- app/Http/Controllers/weixin/PayController.php
+++ app/Http/Controllers/weixin/PayController.php
@@ -56,7 +56,7 @@ class PayController extends Controller
 //		echo 'prepay_id: '.$data->prepay_id;echo '<br>';
 //		echo 'trade_type: '.$data->trade_type;echo '<br>';
         include "phpqrcode/phpqrcode.php";
-        $file_name='erweima/'.$erweima.'.png';
+        $file_name='qrcode/'.$erweima.'.png';
         \QRcode::png($data->code_url,$file_name,'H','5','1');
         echo '<img src="http://1807.96myshop.cn/'.$file_name.'">';die;
 //        echo 'code_url: '.$data->code_url;echo '<br>';

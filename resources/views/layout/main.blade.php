47a2ed84b9184a0d304cb51e28dc125849d9f309
qiangshenderongyao <2302938274@qq.com>
2019-02-27 00:38:35 +0000

    a

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -6,6 +6,7 @@
 	<link rel="stylesheet" href="{{URL::asset('/css/test.css')}}">
 	<meta name="csrf-token" content="{{csrf_token()}}">
 	<link rel="stylesheet" href="{{URL::asset('/bootstrap/css/bootstrap.min.css')}}">
+	<link rel="shortcut icon" href="#" />
 </head>
 <body>
 <div class="container">

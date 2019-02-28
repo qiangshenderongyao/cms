e506ed415d1a4042d40981bfd384227bcf359e09
qiangshenderongyao <2302938274@qq.com>
2019-02-28 07:40:57 +0000

    a

--- app/Http/Controllers/weixin/WxloginController.php
+++ app/Http/Controllers/weixin/WxloginController.php
@@ -44,7 +44,7 @@ class WxloginController extends Controller{
         //查询数据库是否有此人数据
         $pat=WxloginModel::where(['unionid'=>$user_arr['unionid']])->first();
         if($pat['unionid']==$user_arr['unionid']){
-            return redirect('/center');
+            return redirect('/centeradd');
         }else {
             $data = [
                 'nickname' => $user_arr['nickname'],

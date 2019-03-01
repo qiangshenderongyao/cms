c725ebfa0227797deb6d5981367494b1a11e352b
qiangshenderongyao <2302938274@qq.com>
2019-03-01 07:57:54 +0000

    a

--- resources/views/weixin/jssdk.blade.php
+++ resources/views/weixin/jssdk.blade.php
@@ -1,6 +1,7 @@
 <h2>JS SDK</h2>
 <button id="btn">图片吧</button>
 <button id="btn1">扫一扫</button>
+<button id="btn2">地理位置</button>
 <script src="{{URL::asset('/js/jquery-1.12.4.min.js')}}"></script>
 <script src="http://res2.wx.qq.com/open/js/jweixin-1.4.0.js"></script>
 
@@ -11,7 +12,7 @@
         timestamp: {{$jsconfig['timestamp']}}, // 必填，生成签名的时间戳
         nonceStr: "{{$jsconfig['noncestr']}}", // 必填，生成签名的随机串
         signature: "{{$jsconfig['sign']}}",// 必填，签名
-        jsApiList: ['chooseImage','uploadImage','getLocalImgData','startRecord','scanQRCode'] // 必填，需要使用的JS接口列表
+        jsApiList: ['chooseImage','uploadImage','getLocalImgData','startRecord','scanQRCode','openLocation'] // 必填，需要使用的JS接口列表
     });
     wx.ready(function() {
         $("#btn").click(function () {
@@ -32,6 +33,16 @@
                     var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
                 }
             });
-        })
+        });
+        $("#btn2").click(function () {
+            wx.openLocation({
+                latitude: 0, // 纬度，浮点数，范围为90 ~ -90
+                longitude: 0, // 经度，浮点数，范围为180 ~ -180。
+                name: '', // 位置名
+                address: '', // 地址详情说明
+                scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
+                infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
+            });
+        });
     })
 </script>

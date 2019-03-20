8ee2c12162794eaaf689cc02aa2a75e06948fc91
qiangshenderongyao <2302938274@qq.com>
2019-03-20 07:02:15 +0000

    a

--- routes/web.php
+++ routes/web.php
@@ -109,4 +109,4 @@ Route::get('/wxtoken','weixin\KaoshiController@access_token');
 Route::get('/weixin/list','weixin\KaoshiController@weixinlist');
 Route::post('/weixin/listadd','weixin\KaoshiController@weixinlistadd');
 
-Route::get('/hbuired/api','test\TestController@test1');
+Route::post('/hbuired/api','test\TestController@test1');

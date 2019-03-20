3bbbc1a0d9d5c52e73a7bdf219376b9b5a002e2f
qiangshenderongyao <2302938274@qq.com>
2019-03-20 06:22:10 +0000

    a

---
+++ app/Http/Controllers/test/TestController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Http\Controllers\weixin;
+use GuzzleHttp;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+class TestController extends Controller{
+    public function test1(){
+        $data=[
+            'name'=>'枪神',
+            'age'=>20
+        ];
+        return $data;
+    }
+}
+?>

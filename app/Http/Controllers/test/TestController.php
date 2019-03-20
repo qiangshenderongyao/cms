b6ccce53c700c092038e1d221611b1d8c90f1ec2
qiangshenderongyao <2302938274@qq.com>
2019-03-20 06:33:35 +0000

    ab

--- app/Http/Controllers/test/TestController.php
+++ app/Http/Controllers/test/TestController.php
@@ -1,6 +1,5 @@
 <?php
-namespace App\Http\Controllers\weixin;
-use GuzzleHttp;
+namespace App\Http\Controllers\test;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 class TestController extends Controller{

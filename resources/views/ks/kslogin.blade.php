964fccb68f8b0e737457825e4053138a1a845db5
qiangshenderongyao <2302938274@qq.com>
2019-04-02 01:04:01 +0000

    a

--- resources/views/ks/kslogin.blade.php
+++ resources/views/ks/kslogin.blade.php
@@ -8,10 +8,17 @@
                 <td>用户名</td>
                 <td><input type="text" name="sname" class="form-control" placeholder="nickname" required autofocus></td>
             </tr>
+            <tr>
             <td>身份证号</td>
             <td><input type="password" name="password" class="form-control" placeholder="***" required></td>
+            </tr>
+            <tr>
             <td>上传身份证照片</td>
-            <td><input type="file" ></td>
+            <td><input type="file" name="file" class="form-control"></td>
+            </tr>
+            <tr>
+                <td>接口用途</td>
+                <td><input type="text" class="form-control" name="yt"></td>
             </tr>
             <tr>
                 <td><button class="btn btn-lg btn-primary btn-block" type="submit">登录</button></td>

5a831df885bdc74954407c5a42b66bedad57bcfd
qiangshenderongyao <2302938274@qq.com>
2019-04-05 07:21:07 +0000

    a

--- app/Http/Middleware/CheckRequest.php
+++ app/Http/Middleware/CheckRequest.php
@@ -82,9 +82,9 @@ class CheckRequest
                     'data' => []
                 ];
             }
-            //var_dump($this->_api_data);
+            var_dump($this->_api_data);
 
-            //var_dump($map);exit;
+            var_dump($map);exit;
             //生成服务端签名
             ksort($this->_api_data);
             http_build_query($this->_api_data);

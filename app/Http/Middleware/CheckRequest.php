4da654ca977521ef2beace1aebe7f19bceffbb3a
qiangshenderongyao <2302938274@qq.com>
2019-04-08 03:35:07 +0000

    a

--- app/Http/Middleware/CheckRequest.php
+++ app/Http/Middleware/CheckRequest.php
@@ -42,11 +42,11 @@ class CheckRequest
         //把解密的数据传递到控制器
         $request->request->replace($this->_api_data);
         //判断签名是否正确
-//        if ($data['status'] == 1000) {
-//            return $next($request);
-//        } else {
-//            return response($data);
-//        }
+        if ($data['status'] == 1000) {
+            return $next($request);
+        } else {
+            return response($data);
+        }
 
     }
 
